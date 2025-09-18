8abac9c6ddbc4f3136eb1b024fc2e653c4dfea58
Haroon Shafiq <haroonhamza157@gmail.com>
2025-09-18 17:24:50 +0500

    updated code

--- app/nett-pharmacy-case-study/sections/SkinProcess.js
+++ app/nett-pharmacy-case-study/sections/SkinProcess.js
@@ -35,7 +35,8 @@ export default function SkinProcess() {
   return (
  <section className="CommunityService px-[20px] bg-[#f6f6f667] sm:px-[40px] lg:px-0 ">
    <div className="pt-[50px] w-[100%] max-w-[1440px]  mx-auto  pb-[60px] ">
-    <h2 className="text-[24px] sm:text-[28px] md:text-[32px]  lg:text-[36px] text-center text-[#333] raleway font-[600]  sm:leading-[60px]  ">What we did in the Process</h2>
+    <h2 className="text-[24px] sm:text-[28px] md:text-[32px]  lg:text-[36px] text-center text-[#333] raleway font-[600]  sm:leading-[60px]  ">What We Did – The Process & Traits
+</h2>
     <div className="flex  flex-wrap justify-evenly gap-x-[12px] gap-y-[125px] pt-[145px] ">
           {SkinCardsData.map((card, index) => (
             <ServiceCard key={index} {...card}  className={"skinCard !w-[375px] !h-[253px] md:!h-[240px] md:!w-[351px] md:pb-[20px] lg:!h-[250px] lg:!w-[29%]  "} desStyle="text-[16px]  my-[auto] mb-[10px] text-[#575757] mt-[-10px] font-normal leading-[20px] text-center md:px-[0] !px-[22px] md:max-w-[100%]  max-w-[100%] pb-[40px] !text-wrap" imgClass={"mt-[-40px]"} />
